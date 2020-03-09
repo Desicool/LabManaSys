117c7f7283edc3bb35f10addd9f52688ac7aa97f
t-chyao <609936947@qq.com>
2020-03-09 13:51:38 +0800

    first commit

---
+++ LabManagement/DAO/LabContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabManagement.DAO
+{
+    public class LabContext : DbContext
+    {
+    }
+}
---
+++ LabManagement/Utils/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabManagement.Utils
+{
+    public class State
+    {
+        public enum Test { None = 0, processing }
+        void solve()
+        {
+
+        }
+    }
+}
